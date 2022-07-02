7f5b69e4d9a58223ec950833de971d3ffcdac1e2
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-01 21:19:53 -0500

    Update mapper.py
    Commit manual mapper punto 02

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+for row in sys.stdin:
+
+    data = "\t".join(row.split(",")[3:5])
+
+    sys.stdout.write('{}\n'.format(data))
