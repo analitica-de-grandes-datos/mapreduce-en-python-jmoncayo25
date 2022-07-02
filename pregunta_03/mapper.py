ca5ab1574d1413146b1c2bfd2ccedd0a864ea7c6
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-01 21:21:09 -0500

    Update mapper.py
    Commit manual punto 03 mapper

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+elements = []
+
+def take_element(element):
+
+    return element.split(",")[1]
+
+for row in sys.stdin:
+    elements.append(row)
+
+else:
+    elements = sorted(elements, key = take_element)
+
+    for element in elements:
+        sys.stdout.write(element)
+
+#sys.stdout.write(element)
