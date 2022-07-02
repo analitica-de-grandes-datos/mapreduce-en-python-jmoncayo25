cc659e12636b4fd57432cb772df17dec2f5ee73e
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-01 21:21:52 -0500

    Update reducer.py
    Commit manual punto 03 reducer

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+elements = []
+
+def take_element(element):
+
+    return element.split(",")[1]
+
+for row in sys.stdin:
+    elements.append(row)
+
+else:
+    elements = sorted(elements, key = take_element)
+
+    for element in elements:
+        sys.stdout.write(element)
