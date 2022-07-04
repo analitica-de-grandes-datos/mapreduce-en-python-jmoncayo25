769a026f9a7c28ef18b52b87c422b295d5a3a48e
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-03 20:16:38 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -5,21 +5,19 @@ import sys
 
 if __name__ == "__main__":
 
-    curkey = None
-    total = 0
+    dicc = {}
 
-    for row in sys.stdin:
-
-        key, val = row.split("\t")
+    for line in sys.stdin:
+        key,val = line.split('\t')
         val = int(val)
-
-        if key == curkey:
-            total += val
+        
+        if key not in dicc.keys():
+            dicc[key] = val
+            
         else:
-            if curkey is not None:
-                sys.stdout.write("{}\t{}\n".format(curkey, total))
-
-            curkey = key
-            total = val
-
-    sys.stdout.write("{}\t{}\n".format(curkey, total))
+            if dicc[key] < val:
+                dicc[key] = val
+            else:
+                pass
+    for key, val in dicc.items():
+        sys.stdout.write('{}\t{}\n'.format(key, val))
