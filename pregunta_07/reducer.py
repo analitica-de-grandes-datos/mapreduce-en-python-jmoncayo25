508ec350388cb23289780fd5d7272b3a1e3ba478
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-04 17:08:52 -0500

    Update reducer.py

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    replace=[]
+
+    for line in sys.stdin:
+
+        c1, c2, c3 = line.split(",")
+        c3 = int(c3)
+        aux = (c1, c2, c3)
+        replace.append(aux)
+
+    replace2 = sorted(sorted(replace, key = lambda item: item[2]), key = lambda item: item[0])
+
+    for line in replace2:
+        sys.stdout.write("{}   {}   {}\n".format(line[0], line[1], line[2]))
