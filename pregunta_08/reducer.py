01f41eccc4be142f7047b9bc9b8f78ba451ca058
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-04 17:14:06 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    values = []
+    
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            values.append(val)
+        else:
+
+            if curkey is not None:
+                
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, sum(values), sum(values)/len(values)))
+                values = []
+
+            curkey = key
+            values.append(val)
