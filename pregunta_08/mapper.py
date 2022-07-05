1e7ab94a8af3cb8fb6cb43e5260cd8322086e406
jmoncayo25 <94573254+jmoncayo25@users.noreply.github.com>
2022-07-04 20:02:12 -0500

    Update mapper.py

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,13 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-import sys
-if __name__ == "__main__":
+iimport sys
 
-    replace1 = [line.replace('\n', '') for line in sys.stdin]
-    replace2 = [line.split('   ') for line  in replace1]
-    l1 = [replace2[i][0] for i in range(len(replace2))]
-    v1 = [replace2[i][2] for i in range(len(replace2))]
+if __name__ == '__main__':
 
-    for i in range(len(l1)):
-        sys.stdout.write("{}\t{}\n".format(l1[i], v1[i]))
+  for line in sys.stdin:
+
+    lines = line.strip().replace('-', ' ').split(' ')
+
+    sys.stdout.write("{}\t{}\n".format(lines[0], lines[8]))
+    
